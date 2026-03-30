3bb3194c687ad491c3ed4b89ff19d318ec5de811
Brahms <bramso.brahms@gmail.com>
2026-03-30 23:24:59 +0200

    Create controller to: - find material by ID - find material by category - create a material

--- src/main/java/be/brahms/TFE_RentServe/controller/MaterialController.java
+++ src/main/java/be/brahms/TFE_RentServe/controller/MaterialController.java
@@ -1,13 +1,16 @@
 package be.brahms.TFE_RentServe.controller;
 
 import be.brahms.TFE_RentServe.hateoas.material.MaterialAssembler;
+import be.brahms.TFE_RentServe.hateoas.material.MaterialByIdAssembler;
+import be.brahms.TFE_RentServe.models.dtos.material.MaterialByIdDTO;
 import be.brahms.TFE_RentServe.models.dtos.material.MaterialDTO;
+import be.brahms.TFE_RentServe.models.forms.material.MaterialCreateForm;
 import be.brahms.TFE_RentServe.services.MaterialService;
+import jakarta.validation.Valid;
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,10 +26,12 @@ public class MaterialController {
 
     private final MaterialService materialService;
     private final MaterialAssembler materialAssembler;
+    private final MaterialByIdAssembler materialByIdAssembler;
 
-    public MaterialController(MaterialService materialService, MaterialAssembler materialAssembler) {
+    public MaterialController(MaterialService materialService, MaterialAssembler materialAssembler, MaterialByIdAssembler materialByIdAssembler) {
         this.materialService = materialService;
         this.materialAssembler = materialAssembler;
+        this.materialByIdAssembler = materialByIdAssembler;
     }
 
     /**
@@ -39,5 +44,44 @@ public class MaterialController {
         List<MaterialDTO> materials = materialService.findAllMaterials();
         CollectionModel<MaterialDTO> modelMaterials = materialAssembler.toCollectionModel(materials);
         return ResponseEntity.ok(modelMaterials);
+    }
+
+    /**
+     * Get material's data by his ID
+     *
+     * @param id identifier unique
+     * @return data's the favor
+     */
+    @GetMapping("{id}")
+    public ResponseEntity<EntityModel<MaterialByIdDTO>> findMaterialById(@PathVariable Long id) {
+        MaterialByIdDTO materialId = materialService.findMaterialById(id);
+        EntityModel<MaterialByIdDTO> modelMaterialId = materialByIdAssembler.toModel(materialId);
+        return ResponseEntity.ok(modelMaterialId);
+    }
+
+    /**
+     * Get a list of materials grouped by name of category
+     *
+     * @param nameCategory name of category
+     * @return a list of materials grouped by name of category
+     */
+    @GetMapping("category/{nameCategory}")
+    public ResponseEntity<CollectionModel<MaterialDTO>> findByCategoryName(@PathVariable String nameCategory) {
+        List<MaterialDTO> materials = materialService.findAllMaterialsByCategory(nameCategory);
+        CollectionModel<MaterialDTO> modelMaterials = materialAssembler.toCollectionModel(materials);
+        return ResponseEntity.ok(modelMaterials);
+    }
+
+    /**
+     * Create a new material
+     *
+     * @param form the form to create a new material
+     * @return a new material
+     */
+    @PostMapping("new")
+    public ResponseEntity<EntityModel<MaterialDTO>> createMaterial(@RequestBody @Valid MaterialCreateForm form) {
+        MaterialDTO material = materialService.createMaterial(form);
+        EntityModel<MaterialDTO> modelMaterial = materialAssembler.toModel(material);
+        return ResponseEntity.ok(modelMaterial);
     }
 }
