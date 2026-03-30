34f1e4834ca6e84c1de48f79a9e019e96b82ed40
Brahms <bramso.brahms@gmail.com>
2026-03-30 23:25:15 +0200

    Create a DTO material

---
+++ src/main/java/be/brahms/TFE_RentServe/models/dtos/material/MaterialByIdDTO.java
@@ -0,0 +1,18 @@
+package be.brahms.TFE_RentServe.models.dtos.material;
+
+/**
+ * A Dto (Data Transfer Object) for material information
+ * It contains simple data about the material and his details
+ *
+ * @param id           The unique identifier
+ * @param nameMaterial The name of material
+ * @param isAvailable  The favor is available
+ * @param nameCategory The name of category
+ */
+public record MaterialByIdDTO(
+        Long id,
+        String nameMaterial,
+        boolean isAvailable,
+        String nameCategory
+) {
+}
